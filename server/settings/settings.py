671fc2e3f1bdae801df911285652788d51ac0271
Michael <michaelvoov@yandex.ru>
2025-08-16 19:16:33 +0500

    New BASEDATE POSTGRESQL

--- server/settings/settings.py
+++ server/settings/settings.py
@@ -67,14 +67,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'settings.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'XStackOverFlow',
+        'USER': 'postgres',
+        'PASSWORD': 'cr2032',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
